c97451eadbb461e8b1fd90fc65150b7f5bb9fd63
Ivan Alexandrov <ivan@alexandrov.online>
2019-01-05 14:55:42 +0100

    Home: project previews

---
+++ src/components/ProjectPreview.vue
@@ -0,0 +1,59 @@
+<template>
+  <a href="#" class="project" :style="projectBg">
+    <div class="text">
+      <div class="top">{{ title | truncate(34) }}</div>
+      <div class="bottom">{{ author }} &bull; <span class="date">{{ date }}</span></div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "project",
+  props: ["img", "title", "date", "author"],
+  computed: {
+    projectBg() {
+      return {
+        backgroundImage: `url('${this.img}')`
+      };
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.project {
+  display: block;
+  position: relative;
+  height: 250px;
+  background-size: cover;
+  text-decoration: none;
+  color: #ccc;
+  &:hover {
+    .text {
+      padding: 20px 10px;
+    }
+  }
+}
+.text {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: 300ms;
+  padding: 10px;
+  .top {
+    font-weight: 700;
+    font-size: 1.1em;
+    letter-spacing: 1px;
+    text-transform: capitalize;
+  }
+  .bottom {
+    font-size: 0.9em;
+    color: #bbb;
+  }
+  .date {
+    font-size: 0.9em;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="home">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, cupiditate ipsum quasi enim totam, quia numquam eum debitis maiores eveniet illum magnam facere. Officia amet odit natus ratione sint facere.
+  <div class="container">
+    <section class="home">
+      <div class="projects">
+        <project
+          v-for="(p, i) in projects"
+          :key="i"
+          :title="p.title"
+          :img="p.img"
+          :date="p.date"
+          :author="p.author"
+        />
+      </div>
+      <div class="pagination"></div>
+    </section>
   </div>
 </template>
 
 <script>
+import ProjectPreview from "@/components/ProjectPreview";
+import projects from "@/faker/projects.json";
+
 export default {
   name: "home",
+  data() {
+    return {
+      projects: projects.slice(0, 6)
+    };
+  },
   components: {
-    // HelloWorld
+    project: ProjectPreview
   }
 };
 </script>
+
+<style scoped lang="scss">
+.projects {
+  margin: 30px 0;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+}
+</style>
