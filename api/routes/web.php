08b36b005f014ec62d1729f7f57f7f955732c0ad
Ivan Alexandrov <ivan@alexandrov.online>
2019-01-09 23:09:53 +0100

    automove frontend builds

--- api/routes/web.php
+++ api/routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', 'VueController@index')->where('any', '.*');
 
-Auth::routes();
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
--- web/src/router.js
+++ web/src/router.js
@@ -7,6 +7,7 @@ import Register from "./views/Register.vue";
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: "/",
