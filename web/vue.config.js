cbaedd1eb030f6963779c2630563d5b53c294488
Ivan Alexandrov <ivan@alexandrov.online>
2019-01-18 22:24:51 +0100

    dev optimizations

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,9 +1,23 @@
+const apiUrl = require('./package.json').apiUrl;
+
 module.exports = {
   css: {
     loaderOptions: {
       sass: {
         data: '@import "src/styles/_global.scss";',
+      }
+    }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: apiUrl,
+        secure: false
       },
-    },
+      '/p': {
+        target: apiUrl,
+        secure: false
+      }
+    }
   }
 }
